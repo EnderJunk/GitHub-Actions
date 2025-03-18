e45af6b671e11a2a51713e6b08948db927cc3c50
EnderJunk <christian.walters1996@gmail.com>
2025-03-17 22:33:55 -0600

    inital commit

---
+++ cypress.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "cypress";
+import viteConfig from "./vite.config";
+
+export default defineConfig({
+  component: {
+    port: 5173,
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+      viteConfig,
+    },
+  },
+  // Removed e2e configuration
+});
